ebb9f5546a8496847bb705bc93dea22031f6012a
IanShaw027 <131567472+IanShaw027@users.noreply.github.com>
2026-01-05 10:09:38 +0800

    chore(migrations): remove legacy ops archive scripts

--- backend/migrations/archive/v1_ops_legacy_2026-01/030_add_client_ip_index.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
--- 添加客户端 IP 和创建时间复合索引以优化查询性能
-CREATE INDEX IF NOT EXISTS idx_ops_error_logs_client_ip_time
-ON ops_error_logs(client_ip, created_at DESC);
-
--- +goose Down
-DROP INDEX IF EXISTS idx_ops_error_logs_client_ip_time;
